9691085a1aec973127c7af54e10f0ae8e028be6b
Jose F. Ruiz Zamora <josef@iMac-de-Jose.local>
2019-01-15 03:44:51 -0800

    Proyecto inicializado

---
+++ MapSwift/MapSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapSwift
+//
+//  Created by Jose F. Ruiz Zamora on 15/1/19.
+//  Copyright © 2019 Jose F. Ruiz Zamora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
