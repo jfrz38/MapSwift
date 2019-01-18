5779342af8b019fb3f1420a1f32401e082491bcb
Jose F. Ruiz Zamora <josef@iMac-de-Jose.local>
2019-01-18 08:46:13 -0800

    Añadidos métodos iterativos

--- MapSwift/MapSwift/SearchViewController.swift
+++ MapSwift/MapSwift/SearchViewController.swift
@@ -131,6 +131,7 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UIImagePicker
             return
         }
         
+        comprobarBotones()
         //Tiempo
         let inicioTiempoMap = NSDate()
         //Búsqueda Map
@@ -139,13 +140,40 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UIImagePicker
         let diferenciaMap = (finTiempoMap.timeIntervalSinceDate(inicioTiempoMap) * 1000)
         print("Tiempo búsqueda map = ",diferenciaMap)
         
-        print("Resultado = ",resultadosFinalesMap.map({$0.nombre}))
+        //print("Resultado map = ",resultadosFinalesMap.map({$0.nombre}))
         
+        
+        //Tiempo iterativo
+        let inicioTiempoIterativo = NSDate()
+        //Búsqueda iterativa
+        let resultadosFinalesIterar = busquedaBucle()
+        let finTiempoIterativo = NSDate()
+        let diferenciaIterativa = (finTiempoIterativo.timeIntervalSinceDate(inicioTiempoIterativo) * 1000)
+        print("Tiempo búsqueda iterativa = ",diferenciaIterativa)
+        //print("Resultados iterativo = ",resultadosFinalesIterar.map({$0.nombre}))
+        print("Nº resultados = ",resultadosFinalesMap.count)
+        if(resultadosFinalesMap.count == resultadosFinalesIterar.count){
+            var i = 0
+            var distinto = false
+            for p in resultadosFinalesMap{
+                if(p.NIF != resultadosFinalesMap[i].nombre){
+                    print("No coincide")
+                    distinto = true //hay uno distinto
+                    break
+                }
+                i+=1
+                
+            }
+            if(!distinto){
+                print("Coincide")
+            }
+            
+        }else{
+            print("No coincide")
+        }
     }
     
-    //Realiza la búsqueda utilizando Map
-    func busquedaMap() -> [Persona]{
-        
+    func comprobarBotones(){
         //Comprobar los campos de texto
         if(nifTxt.text! == ""){
             opciones[0] = false
@@ -169,6 +197,11 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UIImagePicker
             //TODO : Comprobar si es un número o no
             sueldoInsertado = Double(sueldoTxt.text!)
         }
+    }
+    
+    //Realiza la búsqueda utilizando Map
+    func busquedaMap() -> [Persona]{
+        
         var j = 0
         for op in opciones{
             if(op == true){
@@ -181,6 +214,7 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UIImagePicker
             }
             j+=1
         }
+        
         var resultadosBusqueda = empresa
         var i = 0
         for _ in opciones{
@@ -242,12 +276,7 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UIImagePicker
         }
         
     }
-    
-    //Utiliza la búsqueda iterativamente
-    func busquedaBucle(){
-        
-    }
-    
+
     func filtro_NIF( entrada:[Persona], accion: String, nif: String) -> [Persona]{
         //NIF igual a nif de entrada: devuelve array de objetos personas que coincida
         
@@ -312,4 +341,193 @@ class SearchViewController: UIViewController, UITextFieldDelegate, UIImagePicker
     }
     
     
+    
+    
+    
+    ///PARTE ITERATIVA
+    
+    //Utiliza la búsqueda iterativamente
+    func busquedaBucle() -> [Persona]{
+        
+        var j = 0
+        for op in opciones{
+            if(op == true){
+                break
+            }
+            if(opciones.count-1 == j){
+                //Devuelve un array vacío
+                //TODO indicar que no se ha seleccionado nada
+                return [Persona]()
+            }
+            j+=1
+        }
+        
+        var resultadosBusqueda = empresa
+        var i = 0
+        for _ in opciones{
+            if(opciones[i]==true){
+                resultadosBusqueda = metodoPorPosicionIterar(i,entrada: resultadosBusqueda)
+            }
+            i+=1
+        }
+        
+        if(getSueldoTotal || getSueldoMedio){
+            //Calcular sueldo medio o total de la opción elegida
+            //Se realiza en el mismo if porque para calcular la media se necesita el total
+            
+            //let sueldoMedio = resultadoFinal/Double(resultadosBusqueda.count)
+            
+        }
+        
+        return resultadosBusqueda
+    }
+    
+    //Accede al método iterativo según la posición que esté true
+    func metodoPorPosicionIterar(pos:Int, entrada:[Persona]) -> [Persona]{
+        
+        switch pos {
+        case 0:
+            //NIF igual
+            return filtro_NIF_iterar(entrada,accion: "=",nif: nifInsertado)
+        case 1:
+            //NIF no igual
+            return filtro_NIF_iterar(entrada,accion: "!=",nif: nifInsertado)
+        case 2:
+            //Sueldo mayor
+            return filtro_sueldo_iterar(entrada, accion: ">", sueldo: sueldoInsertado)
+        case 3:
+            //Sueldo menor
+            return filtro_sueldo_iterar(entrada, accion: "<", sueldo: sueldoInsertado)
+        case 4:
+            //Sueldo igual
+            return filtro_sueldo_iterar(entrada, accion: "=", sueldo: sueldoInsertado)
+        case 5:
+            //Becario
+            return filtro_practicas_iterar(entrada,accion: true)
+        case 6:
+            //No becario
+            return filtro_practicas_iterar(entrada,accion: false)
+        case 7:
+            //Nombre igual
+            return filtro_nombre_iterar(entrada,accion: "=",nombre: nombreInsertado)
+        case 8:
+            //Nombre no igual
+            return filtro_nombre_iterar(entrada,accion: "!=",nombre: nombreInsertado)
+        default:
+            print("error")
+            return entrada
+        }
+        
+    }
+    
+    func filtro_NIF_iterar( entrada:[Persona], accion: String, nif: String) -> [Persona]{
+        //NIF igual a nif de entrada: devuelve array de objetos personas que coincida
+        
+        var filtroNIF = [Persona]()
+        
+        if(accion == "="){
+            for p in entrada{
+                if(p.NIF == nif){
+                    filtroNIF.append(p)
+                }
+            }
+        }else{
+            //Accion = !=
+            for p in entrada{
+                if(p.NIF != nif){
+                    filtroNIF.append(p)
+                }
+            }
+            
+        }
+        
+        return filtroNIF
+    }
+    
+    func filtro_sueldo_iterar( entrada:[Persona],accion: String, sueldo:Double) -> [Persona]{
+        
+        var filtroSueldo = [Persona]()
+        
+        if(accion == "<"){
+            for p in entrada{
+                if (p.sueldo < sueldo){
+                    filtroSueldo.append(p)
+                }
+            }
+        }else if(accion == "="){
+            for p in entrada{
+                if (p.sueldo == sueldo){
+                    filtroSueldo.append(p)
+                }
+            }
+        }else{
+            //Accion: >
+            for p in entrada{
+                if (p.sueldo > sueldo){
+                    filtroSueldo.append(p)
+                }
+            }
+        }
+        
+        return filtroSueldo
+    }
+    
+    func filtro_practicas_iterar( entrada:[Persona],accion: Bool) -> [Persona]{
+        
+        var filtroPracticas = [Persona]()
+        
+        if (accion){
+            for p in entrada{
+                if(p.practicas == true){
+                    filtroPracticas.append(p)
+                }
+            }
+        }else{
+            //accion == false
+            for p in entrada{
+                if(p.practicas == false){
+                    filtroPracticas.append(p)
+                }
+            }
+        }
+        
+        return filtroPracticas
+    }
+    
+    func filtro_nombre_iterar( entrada:[Persona],accion: String, nombre:String) -> [Persona]{
+        
+        var filtroNombre=[Persona]()
+        
+        if(accion == "="){
+            for p in entrada{
+                if(p.nombre == nombre){
+                    filtroNombre.append(p)
+                }
+            }
+        }else if(accion == "!="){
+            for p in entrada{
+                if(p.nombre != nombre){
+                    filtroNombre.append(p)
+                }
+            }
+        }else if(accion == "contenga"){
+            for p in entrada{
+                if(p.nombre.containsString(nombre)){
+                    filtroNombre.append(p)
+                }
+            }
+        }else{
+            //accion = no contenga
+            for p in entrada{
+                if(!p.nombre.containsString(nombre)){
+                    filtroNombre.append(p)
+                }
+            }
+        }
+        
+        return filtroNombre
+    }
+
 }
+
+
